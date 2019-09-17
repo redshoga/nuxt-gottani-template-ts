66b86e3ade3d36c274b5c276a67c5c0f427178c8
Yui Ichioka <ichioka.yui@fixer.co.jp>
2019-09-17 18:41:14 +0900

    Add async sample code

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,6 @@ export default {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
     '@nuxtjs/eslint-module',
     '@nuxtjs/style-resources'
   ],
@@ -48,11 +47,6 @@ export default {
     // less: [],
     // stylus: []
   },
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {},
   /*
    ** Build configuration
    */
--- src/pages/use-async.vue
+++ src/pages/use-async.vue
@@ -2,10 +2,34 @@
   <div>
     <h1>Use Async + Axios Sample</h1>
     <div>
-      YES or NO:
+      YES or NO: {{ yesOrNo.answer }}
+    </div>
+    <div>
+      <img :src="yesOrNo.image">
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
+import { axiosClient } from '../plugins/axios'
+
+interface YesOrNo {
+  answer: string,
+  forced: boolean,
+  image: string
+}
+
+interface AsyncData {
+  yesOrNo: YesOrNo
+}
+
+export default Vue.extend({
+  async asyncData(): Promise<AsyncData> {
+    const { data } = await axiosClient.get<YesOrNo>('https://yesno.wtf/api')
+    return {
+      yesOrNo: data
+    }
+  }
+})
 </script>
